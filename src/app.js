c4898671f4ec44bd55b321e05a1c1e36db7f50ea
rayGreycloud <rayGreycloud@gmail.com>
2018-02-24 07:21:21 -0800

    Add submit post handler

--- src/app.js
+++ src/app.js
@@ -3,10 +3,35 @@ import { ui } from './ui';
 
 // Get posts on DOM load 
 document.addEventListener('DOMContentLoaded', getPosts);
+// Add post listener
+document.querySelector('.post-submit')
+  .addEventListener('click', submitPost);
 
+// Fetch posts from api and display
 function getPosts() {
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
     .catch(err => console.log(err));
+}
+
+// submit post handler 
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+  
+  const data = {
+    title, 
+    body
+  }
+  
+  // Create post 
+  http.post('http://localhost:3000/posts', data)
+    .then(data => {
+      ui.showAlert('Post added.', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
+    .catch(err => console.log(err));
+    
 }
 
