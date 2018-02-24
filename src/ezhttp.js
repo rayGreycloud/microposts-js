4afb0a9cdedc38670d93850724d6e06c438b32ab
rayGreycloud <rayGreycloud@gmail.com>
2018-02-24 05:59:03 -0800

    Add http module

---
+++ src/ezhttp.js
@@ -0,0 +1,60 @@
+/** 
+ * ezHTTP Library 
+ * Library for making HTTP requests 
+ * 
+ * @version 3.0.0 
+ * @author rayGreycloud 
+ * @license MIT 
+ *
+ **/
+ 
+class ezHTTP {
+  
+  // GET request 
+  async get(url) {
+    const response = await fetch(url);  
+    const resData =  await response.json();
+    
+    return resData;
+  }
+  
+  // POST request 
+  async post(url, data) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+    const resData = await response.json();
+    
+    return resData;  
+  }
+  
+  // PUT request 
+  async put(url, data) {
+    const response = await fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+    const resData = await response.json();
+    
+    return resData;
+  }
+  
+  // DELETE request 
+  async delete(url) {
+    const response = await fetch(url, {
+        method: 'DELETE'
+      });
+    const resData = await response.json();
+    
+    return resData;
+  }
+}
+
+export default ezHTTP;
